7c6ee62e72746d2679b3cb5f9a1722758e80e72e
Simen Sandvaer <simen@king.com>
2018-05-27 21:09:04 +0200

    smooth

--- Assets/InterpolatedFixedMovement.cs
+++ Assets/InterpolatedFixedMovement.cs
@@ -10,27 +10,26 @@ public class InterpolatedFixedMovement : MonoBehaviour {
 
     private readonly Vector3 simulationMovementPerFixedUpdate = new Vector3(0.1f, 0, 0);
     private readonly Quaternion simluatedRotationPerFixedUpdate = Quaternion.AngleAxis(1f, Vector3.up);
-    private bool pause = false;
-    private float deltaTime = 0;
+    private bool pause = true;
+    private float latestFixed;
 
     void Awake() {
-        Time.timeScale = 0.1f;
+        Time.timeScale = 1f;
     }
     
     
     void Update() {
-        deltaTime += Time.deltaTime;
-        var scale = Mathf.Min(1, deltaTime / Time.fixedDeltaTime);
+        var scale = Mathf.Min(1, (Time.time - latestFixed) / Time.fixedDeltaTime);
         transform.localPosition = Vector3.Lerp(startPoint, destinationPoint, scale);
-        transform.localRotation = Quaternion.Lerp(startRotation, destinationRotation, scale);
     }
 
     void FixedUpdate() {
+        latestFixed = Time.time;
         startPoint = destinationPoint != Vector3.zero ? destinationPoint : transform.localPosition;
         startRotation = destinationRotation != Quaternion.identity ? destinationRotation : transform.localRotation;
         
         destinationPoint = startPoint + simulationMovementPerFixedUpdate;
         destinationRotation = startRotation * simluatedRotationPerFixedUpdate;
-        deltaTime = 0;
+        //Debug.Log("FixedUpdate");
     }    
 }
